934642780a8f871cfc74eec4ed95680c35dcb786
Nicole Simone (RIT Student) <nms6140@boo-1303-11.main.ad.rit.edu>
2018-12-12 06:19:16 -0500

    tests

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,10 +1,11 @@
-"use strict";
-
 $(function(){
     $(".submenu").hide()
 
 });
 
+//onclick = $(function()){
+                     //$(this).next(".submenu").slideToggle("slow")
+                    // });
 $(".menuitem").click(function(){
     $( this ).next(".submenu").slideToggle("slow")
 });
